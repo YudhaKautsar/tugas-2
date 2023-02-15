05186b2b4b9b74d8c218983b3fecf88965d4fbe9
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:26:06 +0700

    first commit

---
+++ tugas-2.js
@@ -0,0 +1,13 @@
+const number = 1;
+const string = 'String';
+const boolean = true;
+const nil = null;
+var undif;
+const array = [1,2,3];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nil);
+console.log(undif);
+console.log(array);
